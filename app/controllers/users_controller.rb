b64faceb9595f9b6962269e61feb22779ab37353
Vova Bilan <vovabilan89@gmail.com>
2013-02-04 14:24:48 +0200

    remove eror from post/edit, and move Company.all form view to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+    @companies = Company.all
   end
 
   def create
+    @companies = Company.all
     @user = User.new params[:user]
     if @user.save
       redirect_to root_path, :notice => "Success"
